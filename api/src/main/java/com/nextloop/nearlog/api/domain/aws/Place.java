314da19663bc6b6ca9c9c06bc24f304fd706a621
bgpark <bgpark82@gmail.com>
2020-07-23 23:17:24 +0900

    feat : Create communication test with native

---
+++ api/src/main/java/com/nextloop/nearlog/api/domain/aws/Place.java
@@ -0,0 +1,13 @@
+package com.nextloop.nearlog.api.domain.aws;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Place {
+
+    private String message;
+    private String latitude;
+    private String longitude;
+}
--- api/src/main/java/com/nextloop/nearlog/api/domain/aws/S3Controller.java
+++ api/src/main/java/com/nextloop/nearlog/api/domain/aws/S3Controller.java
@@ -1,26 +1,26 @@
 package com.nextloop.nearlog.api.domain.aws;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1")
 public class S3Controller {
 
     private final S3Uploader s3Uploader;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-    @PostMapping("/api/v1/upload")
-    @ResponseBody
+    @PostMapping("/upload")
     public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
         return s3Uploader.upload(multipartFile, "static");
     };
+
+    @PostMapping("/native")
+    public String upload(@RequestBody Place place) {
+        System.out.println(place);
+        return "message from server";
+    }
 }
